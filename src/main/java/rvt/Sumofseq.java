4fc9dcd8a5928202755beb0a4c34bf4d2be45148
24DP3LStra <A240886LS@rvt.lv>
2025-10-08 14:56:38 +0300

    Kludas labojums

--- src/main/java/rvt/Sumofseq.java
+++ src/main/java/rvt/Sumofseq.java
@@ -6,18 +6,17 @@ public class Sumofseq {
     public static void main(String[] args) {
         // Your code goes here...
 
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Last number? ");
-            int n_number = Integer.valueOf(scanner.nextLine());
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Last number? ");
+        int n_number = Integer.valueOf(scanner.nextLine());
 
-            int sum_n = 0;
-            int sumof_n = 0;
-            for (int number = 0; number < n_number; number += 1){
-                sum_n +=1;
-                sumof_n += sum_n;
-                
-            }
-            System.out.println("The sum is "+sumof_n);
-            scanner.close();
-    }
+        int sumof_n = 0;
+        for (int number = 0; number <= n_number; number += 1){
+
+            sumof_n += number;
+            
+        }
+        System.out.println("The sum is "+sumof_n);
+        scanner.close();
+}
 }
