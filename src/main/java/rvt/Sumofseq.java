623920c9e233555f80a77e6db06726cabbb74aa4
24DP3LStra <A240886LS@rvt.lv>
2025-10-08 14:48:01 +0300

    Sum of a sequence

---
+++ src/main/java/rvt/Sumofseq.java
@@ -0,0 +1,23 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class Sumofseq {
+    public static void main(String[] args) {
+        // Your code goes here...
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Last number? ");
+            int n_number = Integer.valueOf(scanner.nextLine());
+
+            int sum_n = 0;
+            int sumof_n = 0;
+            for (int number = 0; number < n_number; number += 1){
+                sum_n +=1;
+                sumof_n += sum_n;
+                
+            }
+            System.out.println("The sum is "+sumof_n);
+            scanner.close();
+    }
+}
