0d3c418d1fef22a63aa2d4ac0f00984734fd09eb
24DP3LStra <A240886LS@rvt.lv>
2025-10-08 13:52:57 +0300

    Testing

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,7 +1,23 @@
 package rvt;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
         // Your code goes here...
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give a number:");
+        int skaitlis1 = Integer.valueOf(scanner.nextLine());
+
+        if (skaitlis1 > 0) {
+            System.out.println("The number is positive.");
+
+        }
+        else {
+            System.out.println("The number is negative.");
+        }
+        scanner.close();
     }
 }
