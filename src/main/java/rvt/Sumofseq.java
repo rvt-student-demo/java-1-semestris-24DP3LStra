44befcd05d53658ef3ad10c6dff393d2fd089f4a
24DP3LStra <A240886LS@rvt.lv>
2025-10-08 15:00:33 +0300

    Sum of a sequence - the sequel

--- src/main/java/rvt/Sumofseq.java
+++ src/main/java/rvt/Sumofseq.java
@@ -7,13 +7,17 @@ public class Sumofseq {
         // Your code goes here...
 
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("First number? ");
+        int n_number1 = Integer.valueOf(scanner.nextLine());
+
         System.out.println("Last number? ");
         int n_number = Integer.valueOf(scanner.nextLine());
 
         int sumof_n = 0;
-        for (int number = 0; number <= n_number; number += 1){
+        for (int i = n_number1; i <= n_number; i += 1){
 
-            sumof_n += number;
+            sumof_n += i;
             
         }
         System.out.println("The sum is "+sumof_n);
