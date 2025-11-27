cdb3a1be2439b51a06c5a2c4f52cc6da8249d6af
24DP3LStra <A240886LS@rvt.lv>
2025-11-27 11:32:44 +0200

    Payment card(6 parts) pabeigta versija

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -4,16 +4,38 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        PaymentCard card = new PaymentCard(5);
-        System.out.println(card);
+        
+    // Student st1 = new Student("Jack");
+        PaymentCard paulsCard = new PaymentCard(20);
+        PaymentCard mattsCard = new PaymentCard(30);
 
-        card.eatHeartily();
-        System.out.println(card);
 
-        card.eatHeartily();
-        System.out.println(card);
+        paulsCard.eatHeartily();
+        System.out.println("Paul: "+ paulsCard);
+        mattsCard.eatAffordably();
+        System.out.println("Matt: "+ mattsCard);
+        paulsCard.addMoney(20);
+        System.out.println("Paul: "+ paulsCard);
+        mattsCard.eatHeartily();
+        System.out.println("Matt: "+ mattsCard);
+        paulsCard.eatHeartily();
+        System.out.println("Paul: "+ paulsCard);
+        mattsCard.addMoney(50);
+        System.out.println("Matt: "+ mattsCard);
     }
 }
+
+    // System.out.println(st1.name);
+    // // st1.group = "DP2-1";
+    // System.out.println(st1.group);
+    // Student st2 = new Student("Mike");
+    // System.out.println(st2.name);
+    // Student st3 = new Student("Jordan");    
+    // System.out.println(st3.name);
+
+
+//     }
+// }
 
     //     Scanner scanner = new Scanner(System.in);
 
@@ -43,4 +65,3 @@ public class App {
     //     System.out.println("Sum of even numbers: "+ evenStat.sum());
     //     System.out.println("Sum of odd numbers: "+unevenStat.sum());
     // }
-
--- src/main/java/rvt/PaymentCard.java
+++ src/main/java/rvt/PaymentCard.java
@@ -2,6 +2,8 @@ package rvt;
 
 public class PaymentCard {
     private double balance;
+    private double amount;
+
 
     public PaymentCard(double openingBalance) {
         this.balance += openingBalance;
@@ -9,21 +11,17 @@ public class PaymentCard {
     }
 
     public void eatAffordably() {
-        if (balance < 2.60){
-
+        if (balance >= 2.60){
+            balance-= 2.60;
         }
-        
-        else{
-            this.balance -= 2.60;}
+
     }
 
     public void eatHeartily() {
-        if (balance < 4.60){
-            this.balance -= 2.60;
-        } 
-        else{
+        if (balance >= 4.60){
             this.balance -= 4.60;
-        }
+        } 
+
     }
 
 
@@ -31,4 +29,13 @@ public class PaymentCard {
         return "The card has a balance of " + balance + " euros";
     }
 
+    public void addMoney(double amount){
+        if (amount <= 0){
+        ;
+        }
+
+        else {
+            balance += amount;
+        }
+    }
 }
